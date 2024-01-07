011e3356f139fe9218d3e12c8645a66fdb74108d
VinayAmbatkar <Vinayambatkar57@gmail.com>
2024-01-07 22:32:37 +0530

    File shared

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Hero from "./Components/Hero";
+import Navigation from "./Components/Navigation";
+const App = () => {
+
+  return <div>
+
+    <Navigation />
+    <Hero/>
+  </div>;
+
+
+
+
+
+
+
+
+
+
+};
+export default App;
